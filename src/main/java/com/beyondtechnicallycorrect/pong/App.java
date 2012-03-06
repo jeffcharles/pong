efe6f013b25ee9e8a7d433959f5550404a1511d1
Jeffrey Charles <jeffreycharles@gmail.com>
2012-03-05 20:02:15 -0500

    Add extremely basic GUI setup

--- src/main/java/com/beyondtechnicallycorrect/pong/App.java
+++ src/main/java/com/beyondtechnicallycorrect/pong/App.java
@@ -1,13 +1,23 @@
 package com.beyondtechnicallycorrect.pong;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import javax.swing.JFrame;
+
+public class App {
+
+	public static void main( String[] args ) {
+		javax.swing.SwingUtilities.invokeLater(
+				new Runnable() {
+					public void run() {
+						setupGui();
+					}
+			});
+	}
+	
+	private static void setupGui() {
+		JFrame frame = new JFrame("Pong");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
 }
